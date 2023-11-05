bd16b80d08785f639195be91d3ac20c8de2052df
otreblan <otreblain@gmail.com>
2023-11-05 15:24:47 -0500

    Update finetuning

--- scripts/train-es-finetuned.sh
+++ scripts/train-es-finetuned.sh
@@ -14,18 +14,11 @@ cd ~/donut
 module load cuda/12.2
 module load python/3.9.18
 
-#python3 -m venv --system-site-packages donut-venv
-
-#source donut-venv/bin/activate
-
-# https://github.com/clovaai/donut/issues/192
-#pip3 install donut-python gradio timm==0.5.4 pytorch-lightning==1.8.5 transformers==4.25.1
-
 python3.9 train.py \
 	--config config/train_es_finetuned.yaml \
 	--pretrained_model_name_or_path "result/train_es/synthdog" \
 	--dataset_name_or_paths '["glue-output"]' \
 	--exp_version "docvqa"
 
-module unload cuda/12.2
 module unload python/3.9.18
+module unload cuda/12.2
--- scripts/train-es.sh
+++ scripts/train-es.sh
@@ -15,20 +15,11 @@ cd ~/donut
 module load cuda/12.2
 module load python/3.9.18
 
-#python3 -m venv --system-site-packages donut-venv
-
-#source donut-venv/bin/activate
-
-# https://github.com/clovaai/donut/issues/192
-#pip3 install donut-python gradio timm==0.5.4 pytorch-lightning==1.8.5 transformers==4.25.1
-
 python3.9 train.py \
 	--config config/train_es.yaml \
 	--pretrained_model_name_or_path "naver-clova-ix/donut-base" \
 	--dataset_name_or_paths '["synthdog/outputs/SynthDoG_es"]' \
 	--exp_version "synthdog"
-	#--pretrained_model_name_or_path "naver-clova-ix/donut-base-finetuned-docvqa" \
-	#--exp_version "es"
 
-module unload cuda/12.2
 module unload python/3.9.18
+module unload cuda/12.2
