23af09015a4dd430e6e3efad34c6743a2580c58b
otreblan <otreblain@gmail.com>
2023-11-05 14:18:39 -0500

    Update scripts

--- scripts/train-es-finetuned.sh
+++ scripts/train-es-finetuned.sh
@@ -5,7 +5,6 @@
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:2
 #SBATCH --mem-per-cpu=6G
-#SBATCH --cpus-per-task=8
 #SBATCH --mail-type=ALL
 
 nvidia-smi
@@ -23,12 +22,10 @@ module load python/3.9.18
 #pip3 install donut-python gradio timm==0.5.4 pytorch-lightning==1.8.5 transformers==4.25.1
 
 python3.9 train.py \
-	--config config/train_es.yaml \
-	--pretrained_model_name_or_path "naver-clova-ix/donut-base" \
-	--dataset_name_or_paths '["synthdog/outputs/SynthDoG_es"]' \
-	--exp_version "synthdog"
-	#--pretrained_model_name_or_path "naver-clova-ix/donut-base-finetuned-docvqa" \
-	#--exp_version "es"
+	--config config/train_es_finetuned.yaml \
+	--pretrained_model_name_or_path "result/train_es/synthdog" \
+	--dataset_name_or_paths '["glue-output"]' \
+	--exp_version "docvqa"
 
 module unload cuda/12.2
 module unload python/3.9.18
